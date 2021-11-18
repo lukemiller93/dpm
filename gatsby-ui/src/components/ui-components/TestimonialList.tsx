c1b03c11dcec8a679ffdd32b36d27ed627383ca1
Luke Miller <lukem1293@gmail.com>
2021-11-18 16:42:10 -0600

    chore: :monocle_face: started testimonial list from google

--- gatsby-ui/src/components/ui-components/TestimonialList.tsx
+++ gatsby-ui/src/components/ui-components/TestimonialList.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
+
 export default function TestimonialList() {
-  return (
-    <div>
-      Testimonial list
-    </div>
-  )
+  const [placeData, setPlaceData] = useState(null);
+
+  // Todo: get setup with google business api so I can get reviews to pull in.
+  /*   useEffect(() => {
+    const data = fetch(
+      'https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJw8O87SD3Mq8RiXX-e_QjkOw&fields=name%2Crating%2Cformatted_phone_number&key=AIzaSyDhhYFaHnHcpC0O_UtJRps6gMuRkI_-OIw'
+    ).then((res) => {
+      console.log(res);
+      setPlaceData(res);
+    });
+  }, []); */
+
+  console.log(placeData);
+  return <div className="container">Testimonial list</div>;
 }
