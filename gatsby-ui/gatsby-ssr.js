ec05ec670a70a555bd340d2a7b8b5123c51c9e93
Luke Miller <lukem1293@gmail.com>
2021-11-30 17:05:02 -0600

    still trying to pull in google reviews

--- gatsby-ui/gatsby-ssr.js
+++ gatsby-ui/gatsby-ssr.js
@@ -1,1 +1,13 @@
-export { wrapRootElement } from "./gatsby-browser";
+export { wrapRootElement } from './gatsby-browser';
+
+export function onRenderBody({ setHeadComponents }) {
+  setHeadComponents([
+    <script
+      async
+      defer
+      key="reviews"
+      type="text/javascript"
+      src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`}
+    />,
+  ]);
+}
--- gatsby-ui/src/components/ui-components/TestimonialList.tsx
+++ gatsby-ui/src/components/ui-components/TestimonialList.tsx
@@ -1,18 +1,50 @@
-import { useEffect, useState } from 'react';
+import { google } from 'googleapis';
+import React, { useEffect, useRef, useState } from 'react';
 
+import { Helmet } from 'react-helmet';
+
+const oauth2Client = new google.auth.GoogleAuth({
+  keyFile: '../../utils/dpm_keyfile.json',
+  scopes: [],
+}).getClient();
+
+google.options({ auth: oauth2Client });
 export default function TestimonialList() {
   const [placeData, setPlaceData] = useState(null);
 
-  // Todo: get setup with google business api so I can get reviews to pull in.
-  /*   useEffect(() => {
-    const data = fetch(
-      'https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJw8O87SD3Mq8RiXX-e_QjkOw&fields=name%2Crating%2Cformatted_phone_number&key=AIzaSyDhhYFaHnHcpC0O_UtJRps6gMuRkI_-OIw'
-    ).then((res) => {
-      console.log(res);
-      setPlaceData(res);
-    });
-  }, []); */
+  const reviewDiv = useRef(null);
 
-  console.log(placeData);
-  return <div className="container">Testimonial list</div>;
+  const getReviews = async () => {
+    const businessInfo = google.mybusinessbusinessinformation('v1');
+    const res = await businessInfo.locations.get({
+      name: 'locations/2059534389366578947',
+      readMask: 'reviews',
+    });
+    console.log(res.data);
+  };
+
+  // Todo: get setup with google business api so I can get reviews to pull in.
+  useEffect(() => {
+    getReviews();
+
+    // const data = fetch(
+    //   'https://mybusiness.googleapis.com/v4/accounts/101114181591242164866/locations/2059534389366578947/reviews',
+    //   {
+    //     headers: {
+    //       Authorization:
+    //         'Bearer 291845577769-hc1aebl4dm06q4prs4mgl02i4cqnk3n9.apps.googleusercontent.com',
+    //     },
+    //   }
+    // ).then((res) => {
+    //   console.log(res.json());
+    //   setPlaceData(res);
+    // });
+  }, []);
+
+  return (
+    <>
+      <div id="review-div" ref={reviewDiv} />
+      <div className="container">Testimonial list</div>
+    </>
+  );
 }
