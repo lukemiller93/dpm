8e35ea132bd1d443520729462817104621aefe9b
Luke Miller <lukem1293@gmail.com>
2022-01-07 15:24:31 -0600

    edited gatsby-ssr.js

--- gatsby-ui/gatsby-ssr.js
+++ gatsby-ui/gatsby-ssr.js
@@ -1,13 +1,13 @@
 export { wrapRootElement, wrapPageElement } from './gatsby-browser';
 
-export function onRenderBody({ setHeadComponents }) {
-  setHeadComponents([
-    <script
-      async
-      defer
-      key="reviews"
-      type="text/javascript"
-      src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`}
-    />,
-  ]);
+// export function onRenderBody({ setHeadComponents }) {
+//   setHeadComponents([
+//     <script
+//       async
+//       defer
+//       key="reviews"
+//       type="text/javascript"
+//       src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`}
+//     />,
+//   ]);
 }
